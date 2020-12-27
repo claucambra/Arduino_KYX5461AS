59b4d423ee52a822b9868739a4b79c4278007855
elChupaCambra <claudio.cambra@gmail.com>
2020-12-27 16:38:05 +0100

    Added example code with library

---
+++ Example_with_Lib/Example_with_Lib.ino
@@ -0,0 +1,13 @@
+#include <KYX5461AS.h>
+
+KYX5461AS screen1(2,3,4,5,6,7,8,9,10,11,12);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int printNums[] = {1,2,3,4};
+  screen1.printAll(printNums);
+}
