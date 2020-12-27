b6f8a87826d264161d2748e468fb4837925e4919
elChupaCambra <claudio.cambra@gmail.com>
2020-12-27 17:51:14 +0100

    Updated example sketch

--- Example_with_Lib/Example_with_Lib.ino
+++ Example_with_Lib/Example_with_Lib.ino
@@ -1,6 +1,6 @@
 #include <KYX5461AS.h>
 
-KYX5461AS screen1(2,3,4,5,6,7,8,9,10,11,12);
+KYX5461AS screen1(2,3,4,5,6,7,8,13,9,10,11,12);
 
 void setup() {
   // put your setup code here, to run once:
